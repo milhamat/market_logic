b301b732aa389393341124a0aed5dc271bc05b76
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2022-03-02 16:49:14 +0800

    first init

---
+++ market_simple.py
@@ -0,0 +1,13 @@
+all_items = [{"item":"susu", "harga":50000}, {"item":"daging", "harga":20000},
+             {"item":"lampu", "harga":15000},{"item":"masker", "harga":25000},
+             {"item":"apel", "harga":30000}]
+             
+promotional_items = [{"item":"susu", "harga":50000},
+                     {"item":"masker", "harga":25000}]
+                     
+#print(type(all_items))
+#print(all_items[0]["item"])
+
+def casier(user_inp):
+	
+	if user_inp.lower() == ""
