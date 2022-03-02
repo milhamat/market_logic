5f7857426d10d3cbee6aa092ad7cd94dda14f0c2
Muhammad Ilham Ashiddiq Tresnawan <m477ashiddiq@gmail.com>
2022-03-02 17:03:48 +0800

    remove coment and make finding algorithm

--- experimen_1.py
+++ experimen_1.py
@@ -4,3 +4,12 @@ all_items = [{"item":"susu", "harga":50000}, {"item":"daging", "harga":20000},
              
 promotional_items = [{"item":"susu", "harga":50000},
                      {"item":"masker", "harga":25000}]
+ 
+ #print(type(all_items))
+#print(all_items[0]["item"])                    
+                     
+inp = input("input your finding : ")
+
+for n in range(len(all_items)):
+	if inp == all_items[n]["item"]:
+		print("available with price : ",all_items[n]["harga"])
--- market_simple.py
+++ market_simple.py
@@ -5,8 +5,6 @@ all_items = [{"item":"susu", "harga":50000}, {"item":"daging", "harga":20000},
 promotional_items = [{"item":"susu", "harga":50000},
                      {"item":"masker", "harga":25000}]
                      
-#print(type(all_items))
-#print(all_items[0]["item"])
 
 def casier(user_inp):
 	try:
